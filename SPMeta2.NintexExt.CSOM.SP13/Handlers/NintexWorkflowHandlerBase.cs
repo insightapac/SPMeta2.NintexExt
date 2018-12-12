0a5f40dbd7d82b3f657d7e2a7409e13b19c40314
Marat Bakirov <mbakirov@hotmail.com>
2018-12-12 23:20:52 +0800

    #9 support for kerberos or other protocols in on prem workflow - thanks to @Uolifry

--- SPMeta2.NintexExt.CSOM.SP13/Handlers/NintexWorkflowHandlerBase.cs
+++ SPMeta2.NintexExt.CSOM.SP13/Handlers/NintexWorkflowHandlerBase.cs
@@ -17,32 +17,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Runtime.Serialization.Json;
+using System.Security;
 using SPMeta2.Common;
 using SPMeta.NintexExt.CSOM.SP13.NintexWorkflowWS;
 using System.ServiceModel;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace SPMeta2.NintexExt.CSOM.SP13.Handlers
 {
     public abstract class NintexWorkflowHandlerBase : CSOMModelHandlerBase
     {
-        [DataContract]
-        public class NintexListWorkflowSerialize
-        {
-            [DataMember]
-            public string listId { get; set; }
-
-            [DataMember]
-            public string WorkflowXml { get; set; }
-
-            public static NintexListWorkflowSerialize FromDefinition(NintexListWorkflowDefinition def, ClientContext ctx, Web web, List list)
-            {
-                var result = new NintexListWorkflowSerialize();
-                result.listId = list.Id.ToString("B");
-                result.WorkflowXml = def.WorkflowXml;
-
-                return result;
-            }
-        }
+        private const string SoapPublishFromNWFXmlRequstTemplate = @"<s:Envelope xmlns:s=""http://schemas.xmlsoap.org/soap/envelope/"">
+                        <s:Body xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+                            <PublishFromNWFXml xmlns=""http://nintex.com"">
+                                <workflowFile>{0}</workflowFile>
+                                <listName>{1}</listName>
+                                <workflowName>{2}</workflowName>
+                                <saveIfCannotPublish>{3}</saveIfCannotPublish>
+                            </PublishFromNWFXml>
+                        </s:Body>
+                    </s:Envelope>";
 
         public override void DeployModel(object modelHost, DefinitionBase model)
         {
@@ -60,101 +55,37 @@ namespace SPMeta2.NintexExt.CSOM.SP13.Handlers
 
             // https://help.nintex.com/en-US/sdks/sdk2013/#Walkthroughs/WK_Prc_NWF2013_exportandpublishaworkflow.htm%3FTocPath%3DNintex%2520Software%2520Development%2520Kit%7CModifying%2520and%2520Extending%2520Nintex%2520Workflow%2520and%2520Forms%7C_____5
 
-
             base.DeployModel(modelHost, model);
 
             var listModelHost = modelHost as ListModelHost;
             var baseModelHost = modelHost as CSOMModelHostBase;
 
             var web = baseModelHost.HostWeb;
-            var list = listModelHost!=null? listModelHost.HostList : null;
+            var list = listModelHost != null ? listModelHost.HostList : null;
             var clientContext = baseModelHost.HostClientContext;
-            string FormDigestValue = clientContext.GetFormDigestDirect().DigestValue;
 
+            var formDigestValue = clientContext.GetFormDigestDirect().DigestValue;
             var publishUrl = UrlUtility.CombineUrl(web.Url, "/_vti_bin/NintexWorkflow/Workflow.asmx");
 
-            // Create the binding as per the settings specified in the Nintex SDK
+            var executor = clientContext.WebRequestExecutorFactory.CreateWebRequestExecutor(clientContext, publishUrl);
+            executor.RequestMethod = "POST";
+            executor.RequestContentType = "text/xml; charset=utf-8";
+            executor.RequestHeaders.Add("X-RequestDigest", formDigestValue);
+            executor.RequestHeaders.Add("SOAPAction", "\"http://nintex.com/PublishFromNWFXml\"");
+            executor.RequestHeaders.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate");
+            executor.WebRequest.AutomaticDecompression = DecompressionMethods.GZip;
 
-            BasicHttpBinding binding = new BasicHttpBinding
-            {               
-
-                Name = "NintexWorkflowWSSoap",
-                CloseTimeout = TimeSpan.FromMinutes(1),
-                OpenTimeout = TimeSpan.FromMinutes(1),
-                ReceiveTimeout = TimeSpan.FromMinutes(10),
-                SendTimeout = TimeSpan.FromMinutes(1),
-                AllowCookies = false,
-                BypassProxyOnLocal = false,
-                HostNameComparisonMode = HostNameComparisonMode.StrongWildcard,
-                MaxBufferSize = 100000000,
-                MaxBufferPoolSize = 100000000,
-                MaxReceivedMessageSize = 100000000,
-                MessageEncoding = WSMessageEncoding.Text,
-                TextEncoding = System.Text.Encoding.UTF8,
-                TransferMode = TransferMode.Buffered,
-                UseDefaultWebProxy = true,       
-            };
-
-            binding.ReaderQuotas.MaxDepth = 32;
-            binding.ReaderQuotas.MaxStringContentLength = 999999999;
-            binding.ReaderQuotas.MaxArrayLength = 16384;
-            binding.ReaderQuotas.MaxBytesPerRead = 4096;
-            binding.ReaderQuotas.MaxNameTableCharCount = 16384;
-
-            if (web.Url.ToLowerInvariant().StartsWith("https://"))
+            var requestStream = executor.GetRequestStream();
+            using (var streamWriter = new StreamWriter(requestStream, Encoding.UTF8))
             {
-                binding.Security.Mode = BasicHttpSecurityMode.Transport;
+                var formatedString = BuildSoapRequest(list, WorkflowModel);
+                streamWriter.Write(formatedString);
             }
-            else
-            {
-                binding.Security.Mode = BasicHttpSecurityMode.TransportCredentialOnly;
-            }
+            requestStream.Close();
 
-            binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Ntlm; // TODO - is this unnecessarily constraining?
-            binding.Security.Transport.ProxyCredentialType = HttpProxyCredentialType.None;
-            binding.Security.Transport.Realm = "";
+            executor.Execute();
 
-            binding.Security.Message.ClientCredentialType = BasicHttpMessageCredentialType.UserName;
-            binding.Security.Message.AlgorithmSuite = System.ServiceModel.Security.SecurityAlgorithmSuite.Default;
-
-            var endpoint = new EndpointAddress(publishUrl);
-
-            NintexWorkflowWSSoapClient soapClient = new NintexWorkflowWSSoapClient(binding, endpoint);
-
-            soapClient.ClientCredentials.Windows.ClientCredential = clientContext.Credentials.GetCredential(new Uri(publishUrl), "");
-            soapClient.ClientCredentials.Windows.AllowNtlm = true; // optional - NOTE - this was copied from Nintex SDK, it's not clear if this is required or helpful
-            soapClient.ClientCredentials.Windows.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Impersonation; //optional - NOTE - this was copied from Nintex SDK, it's not clear if this is required or helpful
-
-            //byte[] workflowAsByteArray = System.Text.UTF8Encoding.UTF8.GetBytes(WorkflowModel.WorkflowXml);
-            
-            // System.Convert.ToBase64String(workflowAsByteArray); // This line was in the Nintex SDK sample but appears to break the process
-
-            var result = soapClient.PublishFromNWFXml(WorkflowModel.WorkflowXml, list!=null? list.Title : null, WorkflowModel.Name, true);
-
-            // TODO - improve the method of constructing the web service call, similar to the below
-
-            //var executor = clientContext.WebRequestExecutorFactory.CreateWebRequestExecutor(clientContext, publishUrl);
-            //executor.RequestContentType = "application/json";
-            //executor.RequestContentType = "application/json; charset=utf-8";
-
-            //executor.RequestHeaders.Add("X-RequestDigest", FormDigestValue);
-            ////executor.RequestHeaders.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate");
-            ////executor.RequestHeaders.Add(HttpRequestHeader.ContentEncoding, "utf-8");
-            //executor.RequestMethod = "POST";
-
-            //var serializedObject = NintexListWorkflowSerialize.FromDefinition(WorkflowModel, clientContext, web, list);
-
-            //var requestStream = executor.GetRequestStream();
-            //DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(NintexListWorkflowSerialize));
-            //ser.WriteObject(requestStream, serializedObject);
-            //requestStream.Close();
-            //executor.Execute();
-            //string result = "";
-
-            //using (StreamReader sr = new StreamReader(executor.GetResponseStream()))
-            //{
-            //    result = sr.ReadToEnd();
-            //}
+            var result = ReadSoapResponse(executor);
 
             InvokeOnModelEvent(this, new ModelEventArgs
             {
@@ -166,7 +97,34 @@ namespace SPMeta2.NintexExt.CSOM.SP13.Handlers
                 ObjectDefinition = WorkflowModel,
                 ModelHost = modelHost
             });
+        }
 
+        private string BuildSoapRequest(List list, NintexWorkflowDefinition workflowModel)
+        {
+            //var encodedXml = workflowModel.WorkflowXml.Replace("&", "&amp;").Replace("<", "&lt;").Replace(">", "&gt;").Replace("\"", "&quot;").Replace("'", "&apos;");
+            var encodedXml = System.Net.WebUtility.HtmlEncode(workflowModel.WorkflowXml);
+            var listTitle = list != null ? list.Title : String.Empty;
+            var workflowName = workflowModel.Name;
+            var saveIfCannotPublish = Boolean.TrueString.ToLower();
+            var request = String.Format(SoapPublishFromNWFXmlRequstTemplate, encodedXml, listTitle, workflowName, saveIfCannotPublish);
+            return request;
+        }
+
+        private string ReadSoapResponse(WebRequestExecutor requestExecutor)
+        {
+            string soapResponse = "";
+            using (StreamReader sr = new StreamReader(requestExecutor.GetResponseStream()))
+            {
+                soapResponse = sr.ReadToEnd();
+            }
+            XmlDocument document = new XmlDocument();
+            document.LoadXml(soapResponse);
+            XmlNamespaceManager manager = new XmlNamespaceManager(document.NameTable);
+            manager.AddNamespace("soap", "http://schemas.xmlsoap.org/soap/envelope/");
+            manager.AddNamespace("ns", "http://nintex.com");
+            var response = document.SelectSingleNode("*/soap:Body/*/ns:PublishFromNWFXmlResult", manager);
+            return response != null ? response.InnerText : soapResponse;
         }
     }
 }
+
